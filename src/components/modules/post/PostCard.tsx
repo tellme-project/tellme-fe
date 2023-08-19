2afef32e574188c5201edfce17f51d6aa88c74fb
Jaycent Gunawan Ongris <jaycent.gunawan@ui.ac.id>
2023-08-19 23:28:06 +0700

    fix: register endpoint, inbox, postcard hover

--- src/components/modules/post/PostCard.tsx
+++ src/components/modules/post/PostCard.tsx
@@ -11,7 +11,7 @@ export const PostCard: React.FC<PostProps> = ({
         from = "Anonymous"
     }
     return (
-        <div className="rounded-lg shadow-lg w-80 p-5 mx-3 mb-3">
+        <div className="rounded-lg shadow-lg w-80 p-5 mx-3 mb-3 hover:scale-105 duration-300">
             <div className="font-bold text-md">{content}</div>
             <div className="text-xs my-1 text-gray-400">{convertIso(createdAt)}</div>
             <div className="text-gray-400 text-sm">Posted by <span className="font-bold">{from}</span></div>
